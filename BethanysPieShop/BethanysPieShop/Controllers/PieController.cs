11004d54e5f83547fa17351eb97e4dfd72460f42
APeev89 <apostol.peev@mail.bg>
2022-05-05 14:39:11 +0300

    crate a PieController

---
+++ BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,21 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepositry;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepositry = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+      public ViewResult List()
+        {
+            return View(_pieRepositry.AllPies);
+        }
+    }
+}
--- BethanysPieShop/BethanysPieShop/Program.cs
+++ BethanysPieShop/BethanysPieShop/Program.cs
@@ -1,6 +1,32 @@
+using BethanysPieShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddControllersWithViews();
+
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
 
 app.Run();
